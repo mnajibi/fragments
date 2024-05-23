4e8108906e40c78a7340b10a511ba70ab1a9caa8
Maryam <71490610+mnajibi@users.noreply.github.com>
2024-05-23 12:10:59 -0700

    se the createSuccessResponse() and createErrorResponse() functions in src/response.js

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,13 +1,8 @@
 // src/routes/api/get.js
-
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 /**
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json({
-    status: 'ok',
-    // TODO: change me
-    fragments: [],
-  });
+  res.status(200).json(createSuccessResponse({ fragments: [] }));
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ const { authenticate } = require('../auth');
 // Create a router that we can use to mount our API
 const router = express.Router();
 
+const { createSuccessResponse } = require('../response');
+
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
@@ -21,13 +23,13 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/mnajibi/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/mnajibi/fragments',
+      version,
+    })
+  );
 });
 
 module.exports = router;
