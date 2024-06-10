fd7172a13ef34a8f42d55c42e22249cba6f10bbb
Maryam <71490610+mnajibi@users.noreply.github.com>
2024-06-09 17:47:21 -0700

    a1 completed

--- package/src/routes/api/get.js
+++
@@ -1,8 +0,0 @@
-// src/routes/api/get.js
-const { createSuccessResponse } = require('../../response');
-/**
- * Get a list of fragments for the current user
- */
-module.exports = (req, res) => {
-  res.status(200).json(createSuccessResponse({ fragments: [] }));
-};
